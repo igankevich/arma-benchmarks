46acdd0ebdbbd7a5ca50b76e7af19997d8f09f1d
Ivan Gankevich <igankevich@ya.ru>
2017-08-04 13:08:39 +0300

    Allow loading non-numeric data.

--- R/arma.load.R
+++ R/arma.load.R
@@ -1,4 +1,8 @@
-arma.load <- function (outdir, hostname, nt, framework, model, tag, regex) {
+arma.load <- function (outdir, hostname, nt, framework, model, tag, regex, ...) {
+	args <- list(...)
+	if (!("numeric" %in% names(args))) {
+		args$numeric = TRUE
+	}
 	dir <- file.path(outdir, hostname, nt, framework, model)
 	files <- sort(list.files(dir, pattern="\\.log$"))
 	message(paste("Reading", dir))
@@ -10,9 +14,11 @@ arma.load <- function (outdir, hostname, nt, framework, model, tag, regex) {
 			tag_list <- paste(tag, collapse="|")
 			tag_list <- paste("(", tag_list, ")", sep="")
 		}
-		lines <- lines[grepl(paste("^prfl", tag_list), lines, perl=TRUE)]
+		lines <- lines[grepl(tag_list, lines, perl=TRUE)]
 		lines <- gsub(regex, "\\1", lines, perl=TRUE)
-		lines <- as.numeric(lines)
+		if (args$numeric) {
+			lines <- as.numeric(lines)
+		}
 		if (length(lines) == 0) {
 			lines <- NA
 		}
